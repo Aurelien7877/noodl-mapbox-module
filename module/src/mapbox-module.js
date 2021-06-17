2233f88d3858bce66a1373d24fe4981d78dee6f9
aurel <aurelienpouxviel@gmail.com>
2021-06-17 16:12:08 +0200

    Version Finale prête à servir

--- module/src/mapbox-module.js
+++ module/src/mapbox-module.js
@@ -2,13 +2,11 @@ const Noodl = require('@noodl/noodl-sdk');
 import {useRef, useEffect} from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+//Importation pour la partie tracer polygone
 import * as turf from '@turf/turf'
-
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
-import DrawRectangle, { //Sert pour la solution alternative de draw rectangle
-	DrawStyles,
-  } from "mapbox-gl-draw-rectangle-restrict-area";
+
 
 //import the css using webpack
 //a very simple react component that tells the caller when it's <div> is mounted and unmounted
@@ -54,7 +52,7 @@ function DivComponent(props) {
 				//clear any previous warnings, if any
 				this.clearWarnings();
 			}
-			//CRéation constante MAP
+			//Création constante MAP
 			mapboxgl.accessToken = accessToken;
 			const map = new mapboxgl.Map({
 				container: domElement,
@@ -65,23 +63,9 @@ function DivComponent(props) {
 			});
 			this.map = map;
 
-			
-			/*map.on('move', () => {
-				this.setOutputs({
-					longitude: map.getCenter().lng.toFixed(4),
-					latitude: map.getCenter().lat.toFixed(4),
-					zoom: map.getZoom().toFixed(2)
-				})
-			});*/
-
-			function drawPolygon() {
-			}
-			this.drawPolygon=drawPolygon;
-	
-
-			//variable de dessin de forme
+			//Fonction de dessin de polygones
 			var draw = new MapboxDraw({
-				displayControlsDefault: false, //J'ai testé en true : bug
+				displayControlsDefault: false,
 				controls: {
 					polygon: true,
 					trash: true
@@ -90,7 +74,7 @@ function DivComponent(props) {
 			});
 			//On add a la map
 			map.addControl(draw);
-		
+			//On crée, supprime, met à jour la forme
 			map.on('draw.create', updateArea);
 			map.on('draw.delete', updateArea);
 			map.on('draw.update', updateArea);
@@ -103,7 +87,7 @@ function DivComponent(props) {
 					var area = turf.area(data);
 					// restrict to area to 2 decimal points
 					var rounded_area = Math.round(area * 100) / 100;
-					//answer.innerHTML ='<p><strong>' + rounded_area + '</strong></p><p>mètres carrés</p>';
+					//answer.innerHTML ='<p><strong>' + rounded_area + '</strong></p><p>mètres carrés</p>'; //FAIS BUGER LA FERMETURE DES FORMES
 				} else {
 					answer.innerHTML = '';
 					if (e.type !== 'draw.delete')
@@ -111,7 +95,7 @@ function DivComponent(props) {
 				}
 			}
 
-			//Fonction de géolocalisation
+			//Fonction de géolocalisation de l'utilisateur
 			this.geolocate = new mapboxgl.GeolocateControl({
 				positionOptions: {
 					enableHighAccuracy: true
@@ -119,7 +103,7 @@ function DivComponent(props) {
 				trackUserLocation: true
 			});
 
-			//Fonction pour ajouter un pin
+			//Fonction pour ajouter un pin sur la carte, aux coord sur NOODL
 			function addPin(long, lat) {
 				var marker = new mapboxgl.Marker()
 					.setLngLat([long, lat])
@@ -127,7 +111,7 @@ function DivComponent(props) {
 			};
 			this.addPin = addPin;
 			
-			//Fonction pour faire apparaitre un PopUp
+			//Fonction pour faire apparaitre un PopUp écrivant les coords (celles de NOODL)
 			function popUp(long,lat) {
 				var fenetre = new mapboxgl.Popup()
 				.setLngLat([long,lat])
@@ -136,14 +120,14 @@ function DivComponent(props) {
 			}
 			this.popUp=popUp;
 
-			//Fonction qui change le style de la carte
+			//Fonction qui change le style de la carte, ne marche pas 
 			function styleChange() {
 
 				this.inputs.mapboxStyle='mapbox://styles/mapbox/satellite-v9'
 			}
 			this.styleChange = styleChange;
 
-			//Fonction pour centrer
+			//Fonction pour centrer la caméra vers les coord rentrées sur NOODL
 			function flyTo(long, lat) {
 				this.map.flyTo({
 					center: [long, lat],
@@ -152,20 +136,15 @@ function DivComponent(props) {
 			}
 			this.flyTo = flyTo;
 
-
-
-			//Fonction pour naviguer fleche
+			//Fonction pour naviguer avec les fleches. MARCHE DE BASE ??
 			function navigate() {
 				// pixels the map pans when the up or down arrow is clicked
 				var deltaDistance = 100;
-
 				// degrees the map rotates when the left or right arrow is clicked
 				var deltaDegrees = 25;
-			
 				function easing(t) {
 					return t * (2 - t);
 				}
-			
 				map.on('load', function () {
 					map.getCanvas().focus();
 			
@@ -203,16 +182,15 @@ function DivComponent(props) {
 			}
 			this.navigate=navigate;
 
-
+			//Fonction qui ajoute 3 boutons de navigations : zoom, dézoom et angles (possible aussi a la souris)
 			map.addControl(new mapboxgl.NavigationControl());
+			//Ajoute la fonction géolocalisation sur la map
 			map.addControl(this.geolocate);
 
 			map.on('load', () => {
 				this.sendSignalOnOutput("mapLoaded");
 			});
-			/*map.on('load', function() {
-				draw.add({});
-			});*/
+
 			  
 		}
 	},
@@ -232,16 +210,16 @@ function DivComponent(props) {
 		latitude: {displayName: 'Latitude', type: 'number', group: 'Coordinates', default: 0},
 		zoom: {displayName: 'Zoom', type: 'number', group: 'Coordinates', default: 0},
 	},
-
+	//Signaux pour faire des send/receive events sur NOODL
 	signals: {
-		centerOnUser: {
+		centerOnUser: { //Pour géolocalisation
 			displayName: 'Center on user',
 			group: 'Actions',
 			signal() {
 				this.geolocate && this.geolocate.trigger();
 			},
 		},
-		addPin: {
+		addPin: {//Ajouter un pin aux coord de NOODL
 			displayName: 'Add pin to map',
 			group: 'Actions',
 			signal() {
@@ -249,7 +227,7 @@ function DivComponent(props) {
 			}
 		},
 
-		flyTo: {
+		flyTo: {//Centrer la carte aux coord de NOODL
 			displayName: 'Pan to location',
 			group: 'Actions',
 			signal() {
@@ -257,15 +235,7 @@ function DivComponent(props) {
 			}
 		},
 
-		drawPolygon: {
-			displayName: 'Draw a polygon',
-			group: 'Actions',
-			signal() {
-				this.drawPolygon();
-			}
-		},
-
-		popUp: {
+		popUp: {//Ajouter une fenetre qui écrit les coords de NOODL
 			displayName: 'Popup Coords',
 			group: 'Actions',
 			signal() {
@@ -273,7 +243,7 @@ function DivComponent(props) {
 			}
 		},
 
-		styleChange: {
+		styleChange: {//Changer la carte en style satellite, ne marche PAS
 			displayName: 'Change to satellite view',
 			group: 'Actions',
 			signal() {
@@ -281,7 +251,7 @@ function DivComponent(props) {
 			}
 		},
 
-		navigate: {
+		navigate: {//Pour naviguer avec les fleches
 			displayName: 'naviguate with -> <-',
 			group: 'Actions',
 			signal() {
